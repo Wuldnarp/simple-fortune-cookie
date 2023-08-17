e0c8a1d808c2e4652d57e41ce88a8b299022898f
Emre Eker <90039700+Wuldnarp@users.noreply.github.com>
2023-08-17 13:42:39 +0200

    6 03 container registry (#12)
    * wait for branch docker to marge into main
    
    * wait for branch docker to marge into main
    
    * Co-authored-by: matni21 <matni21@users.noreply.github.com>
    Co-authored-by: OFHansen <OFHansen@users.noreply.github.com>
    
    * small change with paths in docker build
    
    * fix the path in docker build script
    
    * pt3
    
    * pt3
    
    * pt.5

---
+++ ci/frontend-build-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username_lower}/"
+docker build -t "${DockerRepo}simple-fortune-cookie-frontend-app:latest" -t "${DockerRepo}simple-fortune-cookie-frontend-app:1.0-$Tag" .
